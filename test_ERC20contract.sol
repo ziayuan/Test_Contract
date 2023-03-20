aa7ab7fa819deb97e8c10dc0d21a7ca4c189ffb2
ziayuan <121813574+ziayuan@users.noreply.github.com>
2023-03-20 21:06:13 +0800

    Update test_ERC20contract.sol

--- test_ERC20contract.sol
+++ test_ERC20contract.sol
@@ -1,10 +1,57 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract KLToken {
+    string public name = "KL";
+    string public symbol = "KL";
+    uint256 public totalSupply = 1000000;
+    mapping(address => uint256) balances;
 
-contract KLToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("KL Token", "KL") {
-        _mint(msg.sender, initialSupply);
+    constructor() {
+        balances[msg.sender] = totalSupply;
     }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balances[msg.sender] >= _value);
+        require(_to != address(0));
+
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(balances[_from] >= _value);
+        require(allowances[_from][msg.sender] >= _value);
+        require(_to != address(0));
+
+        balances[_from] -= _value;
+        balances[_to] += _value;
+        allowances[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowances[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return allowances[_owner][_spender];
+    }
+
+    mapping(address => mapping(address => uint256)) allowances;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
